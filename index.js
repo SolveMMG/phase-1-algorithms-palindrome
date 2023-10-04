f33d3692bf8cc946b8f04e7b843abdb69265d2e5
Your Name <mikachupichu@gmail.com>
2023-10-04 18:31:45 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 function isPalindrome(word) {
+  const lowerCase = word.toLowerCase();
+  const reversed = lowerCase.split("").reverse().join("");
+  return reversed === word;
   // Write your algorithm here
 }
 
 /* 
   Add your pseudocode here
+  it takes a word 
+  reverses it 
+  then tests if it is equal to the word inputed
 */
 
 /*
   Add written explanation of your solution here
+  The function takes a word and reverses it to test if the word
+  is a palindrome.
 */
 
 // You can run `node index.js` to view these console logs
